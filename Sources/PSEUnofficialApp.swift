f9777ad4e7167693694884a4d139ee31b0e1fe9f
m4riusz <mariusz.sut.94+github@gmail.com>
2022-01-08 18:25:32 +0100

    init project

---
+++ Sources/PSEUnofficialApp.swift
@@ -0,0 +1,17 @@
+//
+//  PSEUnofficialApp.swift
+//  PSEUnofficial
+//
+//  Created by Mariusz Sut on 08/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct PSEUnofficialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
