dc12550a6d8bb8d80a36974088be19fcfef54437
m4riusz <mariusz.sut.94+github@gmail.com>
2022-02-05 17:19:28 +0100

    minor changes

--- Scripts/Generate.sh
+++ Scripts/Generate.sh
@@ -1,1 +1,2 @@
+rm -rf PSEUnofficial.xcodeproj/project.pbxproj
 xcodegen generate --spec project.json
---
+++ iOS/Tests/iOSTests.swift
@@ -0,0 +1,16 @@
+//
+//  iOSTests.swift
+//  iOSTests
+//
+//  Created by Mariusz Sut on 08/01/2022.
+//
+
+import XCTest
+@testable import iOS
+
+class iOSTests: XCTestCase {
+
+    func testExample() throws {
+        XCTAssertEqual(2 + 2, 4)
+    }
+}
