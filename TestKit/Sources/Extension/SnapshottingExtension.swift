6ddc5309f4df57e50f3c1e259f248f9bb1b56b09
m4riusz <mariusz.sut.94+github@gmail.com>
2022-08-19 10:39:43 +0200

    fix snapshot tests after update

--- TestKit/Sources/Extension/SnapshottingExtension.swift
+++ TestKit/Sources/Extension/SnapshottingExtension.swift
@@ -12,7 +12,8 @@ import UIKit
 extension Snapshotting where Value: SwiftUI.View, Format == UIImage {
     public static func standardImage(size: CGSize = CGSize(width: 375, height: 0),
                                      mode: UIUserInterfaceStyle = .light) -> Snapshotting {
-        .image(precision: 0.99,
+        .image(drawHierarchyInKeyWindow: true,
+               precision: 0.99,
                layout: .fixed(width: size.width, height: size.height),
                traits: UITraitCollection(userInterfaceStyle: mode))
     }
