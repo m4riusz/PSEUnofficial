03b04114e6682791fecaf6f8d4ccd9222f4c833b
m4riusz <mariusz.sut.94+github@gmail.com>
2022-08-19 11:30:04 +0200

    bump simulator versions

--- Scripts/RuniOSUITests.sh
+++ Scripts/RuniOSUITests.sh
@@ -2,5 +2,5 @@ xcodebuild \
 	-project PSEUnofficial.xcodeproj \
 	-scheme iOSUITests \
 	-sdk iphonesimulator \
-	-destination 'platform=iOS Simulator,name=iPhone 8,OS=15.2' \
+	-destination 'platform=iOS Simulator,name=iPhone 8,OS=15.5' \
 	test
