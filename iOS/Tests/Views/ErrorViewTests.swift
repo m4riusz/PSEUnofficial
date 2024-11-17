68fe92b260756cfd0fe48dda97e52cb20a183d84
Mariusz Sut <mariusz.sut.94@gmail.com>
2024-11-17 17:46:22 +0100

    changes

--- iOS/Tests/Views/ErrorViewTests.swift
+++ iOS/Tests/Views/ErrorViewTests.swift
@@ -5,29 +5,24 @@
 //  Created by Mariusz Sut on 19/03/2022.
 //
 
-//import Core
-//import XCTest
-//import SwiftUI
-//import TestKit
-//@testable import iOS
-//
-//final class ErrorViewTests: BaseUnitTest {
-//    private struct Constants {
-//        static let size = CGSize(width: 375, height: 375)
-//    }
-//    private var title = "Title Title Title Title Title Title Title Title Title Title"
-//    private var message = "Message Message Message Message Message Message Message Message Message"
-//    private var action = "ACTION"
-//    private lazy var sut = ErrorView(viewModel: .init(title: title,
-//                                                      message: message,
-//                                                      action: action,
-//                                                      onAction: {}))
-//
-//    func testLightMode() {
-//        assertSnapshot(of: sut, as: .standardImage(size: Constants.size))
-//    }
-//
-//    func testDarkMode() {
-//        assertSnapshot(of: sut, as: .standardImage(size: Constants.size, mode: .dark))
-//    }
-//}
+import TestKit
+import SnapshotTesting
+@testable import iOS
+
+final class ErrorViewTests: BaseUnitTest {
+    private var title = "Title Title Title Title Title Title Title Title Title Title"
+    private var message = "Message Message Message Message Message Message Message Message Message"
+    private var action = "ACTION"
+    private lazy var sut = ErrorView(viewModel: .init(title: title,
+                                                      message: message,
+                                                      action: action,
+                                                      onAction: {}))
+    
+    func testLightMode() {
+        assertSnapshot(of: sut, as: .standardImage())
+    }
+
+    func testDarkMode() {
+        assertSnapshot(of: sut, as: .standardImage(mode: .dark))
+    }
+}
